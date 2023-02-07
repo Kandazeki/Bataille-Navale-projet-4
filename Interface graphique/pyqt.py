90312cbd18973ed9ce9fd838072ad3c84a3f26eb
jujunsi <113674833+jujunsi@users.noreply.github.com>
2023-02-07 15:20:55 +0100

    interface
    j'ai un peu amélioré l'interface il faut encore améliorer la mise en page mais je pense que c'est déjà pas mal

--- Interface graphique/pyqt.py
+++ Interface graphique/pyqt.py
@@ -9,36 +9,44 @@ class Fenetre (QWidget):
         QWidget.__init__(self)
         self.setWindowTitle("Bataille navale")
         self.setGeometry(100, 100, 300, 200)
+        
+        self.label = QLabel("Bienvenue dans notre bataille navale dernière génération\nVeuillez sélectionner un mode de difficulté")
+        
+        self.radiobutton1 = QRadioButton ("Mode facile : Pirate")
+        self.radiobutton1.setChecked(True)
+        self.radiobutton1.setAccessibleName("pirate")
+        self.radiobutton2 = QRadioButton ("Mode normal : présent")
+        self.radiobutton2.setAccessibleName("présent")
+        self.radiobutton3 = QRadioButton ("Mode difficile : futur")
+        self.radiobutton3.setAccessibleName("futur")
+        
+        self.bouton = QPushButton ("Valider")
+
         layout = QVBoxLayout()
+        layout.addWidget(self.label)
+        layout.addWidget(self.radiobutton1)
+        layout.addWidget(self.radiobutton2)
+        layout.addWidget(self.radiobutton3)
+        layout.addWidget(self.bouton)
         self.setLayout(layout)
 
-        self.label = QLabel("Bienvenue dans notre bataille navale dernière génération\nVeuillez sélectionner un mode de difficulté")
-        layout.addWidget(self.label)
-
-        self.radiobutton = QRadioButton ("Mode facile : Pirate")
-        self.radiobutton.setChecked(True)
-        self.radiobutton.toggled.connect(self.onClicked)
-        layout.addWidget(self.radiobutton)
-
-        self.radiobutton = QRadioButton ("Mode normal : présent")
-        self.radiobutton.toggled.connect(self.onClicked)
-        layout.addWidget(self.radiobutton)
-
-        self.radiobutton = QRadioButton ("Mode difficile : futur")
-        self.radiobutton.toggled.connect(self.onClicked)
-        layout.addWidget(self.radiobutton)
-
-        self.bouton = QPushButton ("Valider")
+        self.radiobutton1.toggled.connect(self.onClicked)
+        self.radiobutton2.toggled.connect(self.onClicked)
+        self.radiobutton3.toggled.connect(self.onClicked)
         self.bouton.clicked.connect(self.appui_bouton)
-        layout.addWidget(self.bouton)
 
     def onClicked(self):
         radioButton = self.sender()
         if radioButton.isChecked():
-            print("Vous avez choisi un mode ")
+            print("Vous avez choisi le mode", radioButton.accessibleName())
 
     def appui_bouton(self):
-        print("Vous avez validé")
+        if self.radiobutton1.isChecked():
+            print("Vous avez validé le mode ", self.radiobutton1.accessibleName())
+        elif self.radiobutton2.isChecked():
+            print("Vous avez validé le mode ", self.radiobutton2.accessibleName())
+        elif self.radiobutton3.isChecked():
+            print("Vous avez validé le mode ", self.radiobutton3.accessibleName())
 
 app = QApplication.instance()
 if not app:
