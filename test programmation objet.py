809308cb4338b4e77adc3f35d27ccbb5ee047dce
jujunsi <113674833+jujunsi@users.noreply.github.com>
2023-02-10 11:54:09 +0100

    grille pyqt5
    j'ai réussi à faire une grille mais je ne vois pas pour l'instant comment afficher des bateaux dessus et surtout comment les relier aux bateaux que j'ai crée. Pour l'instant il y a juste plein de boutons qui affiche 'vous avez cliqué' quand on clique dessus...

--- test programmation objet.py
+++ test programmation objet.py
@@ -3,7 +3,7 @@
 # je crée une classe 'Ship'
 class Ship :
     # j'initialise le bateau en donnant son nom, sa taille, 
-    # son arme (et le nombre de fois qu'on peut l'utiliser) et son état
+    # son arme, le nombre de fois qu'on peut l'utiliser et ses points de vie
     def __init__(self, name, size, weapon, number_of_use = 0):
         self.name = name
         self.size = size
@@ -11,15 +11,15 @@ class Ship :
         self.NumberOfUse = number_of_use
         self.life = self.size
 
-    # je diminue de 1 la taille du bateau donc sa vie
+    # je diminue de 1 la vie du bateau
     def touched (self):
         self.life = self.life - 1
 
-    # j'augmente de 1 le nombre d'utilisation
+    # je diminue de 1 le nombre de munition si le bateau peut attaquer
     def useWeapon (self):
         if self.IsAbleToUseWeapon() == True :
             self.NumberOfUse = self.NumberOfUse - 1
-        else :
+        elif self.IsAbleToUseWeapon() == False : 
             print ("Vous ne pouvez pas utiliser votre arme")
 
     # je teste si le bateau est détruit
@@ -30,7 +30,7 @@ class Ship :
         else :
             return False
     
-    #on teste si le bateai peut utiliser son arme
+    # on teste si le bateau peut utiliser son arme (si il n'est pas détruit et qu'il lui reste des munitions)
     def IsAbleToUseWeapon (self):
         if self.NumberOfUse <= 0 or self.life == 0 :
             return False
@@ -42,27 +42,29 @@ Thousand_Sunny = Ship ("Sunny", 3, "coup de burst", 3)
 Vogue_Merry = Ship ("Merry", 2, "barrel", 2)
 Moby_Dick = Ship ("Moby Dick", 5, None)
 
-print (Thousand_Sunny.name, Thousand_Sunny.weapon)
-print (Vogue_Merry.name, Vogue_Merry.weapon)
-print (Moby_Dick.name)
-
 #je teste la fonction 'touched'
 Thousand_Sunny.touched ()
-
 print (Thousand_Sunny.life)
 
-# J'enlève les 2 vies du voque merry pour tester la fonction
+# J'enlève les 2 vies du voque merry pour tester la fonction 'isShipDestroyed'
 Vogue_Merry.touched ()
 Vogue_Merry.touched ()
-
 Vogue_Merry.isShipDestroyed()
 Moby_Dick.isShipDestroyed()
 
+# je teste la fonction qui enlève une munition si le bateau n'est pas détruit et qu'il lui reste des munitions
+print ("\nVous avzez", Thousand_Sunny.NumberOfUse, "munitions")
 Thousand_Sunny.useWeapon()
 print ("il vous reste", Thousand_Sunny.NumberOfUse, 'munitions')
 Thousand_Sunny.useWeapon()
+print ("il vous reste", Thousand_Sunny.NumberOfUse, 'munition')
 Thousand_Sunny.useWeapon()
-print (Thousand_Sunny.NumberOfUse)
-Thousand_Sunny.useWeapon
-print (Thousand_Sunny.NumberOfUse)
-print(Thousand_Sunny.useWeapon)
+print ("il vous reste", Thousand_Sunny.NumberOfUse, 'munition')
+Thousand_Sunny.useWeapon()
+print ("il vous reste", Thousand_Sunny.NumberOfUse, 'munition')
+
+print("\n\nBonjour comme tu l'as vu j'ai un peu avancé cependant mon code n'est pas vraiment utilisable pour le moment"\
+      +" il faudrait en effet avancer dans l'interface graphique avant d'aller plus loin "\
+        + "j'ai essayé de créer une grille dans le fichier grilleinterface mais on ne peut pas faire grand chose avec pour le moment"\
+            +" c'est pourquoi si tu vois ce message il faudrait qu'on s'organise pour voir comment on avance et surtout comment on fait cette putain de grille "\
+                +" \ntrès cordialement,\nta camarade de NSI.")
